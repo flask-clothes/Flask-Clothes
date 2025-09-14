c9734aa8b48f1ed0b53ad0c4c153a38aab424ae3
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2025-09-14 12:22:47 -0300

    feat: banner working

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template, url_for, request, make_response, redir
 
 app = Flask(__name__)
 
+token = "ssrToken"
+
+users = {
+    'joao': "2708",
+    "vih": "0305"
+}
+
 
 class Clothe:
 
@@ -29,6 +36,28 @@ def home():
     return render_template('index.html', clothes=clothes)
 
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    method = request.method
+    if method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        print(password, name)
+        if ((name in users.keys()) and password == users[name]):
+            resp = make_response(redirect(url_for("home")))
+            resp.set_cookie("token", token, 60)
+            return resp
+        msg = "Ops, você errou algum campo, tente novamente"
+
+        return render_template("login.html", msg=msg)
+    return render_template("login.html")
+
+
+@app.route("/")
+def default():
+    return redirect(url_for("login"))
+
+
 @app.route("/ssr/clothe/<id>")
 def info(id):
     print(id)
@@ -43,6 +72,14 @@ def info(id):
             return render_template('clothe.html', clothe=selected_clothe)
 
     return redirect(url_for('home'))
+
+
+@app.route("/deletecookies")
+def deletingcookies():
+    resp = make_response(redirect(url_for("login")))
+    resp.delete_cookie("user")
+    resp.delete_cookie("user")
+    return resp
 
 
 if __name__ == "__main__":
